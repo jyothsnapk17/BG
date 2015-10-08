f8817fbe711135ee9d9bde1be8dbb646896eb778
Jyothsna Kullatira <jyothsnapk17@gmail.com>
2015-10-08 00:31:54 -0700

    1. resolved threading issue for concurrent EJDB connections 2. changed confirmFriend method to add confirmed friend record directly 3. code cleanup

---
+++ db/EJDB/src/EJDBClientProperties.java
@@ -0,0 +1,11 @@
+package src;
+
+public interface EJDBClientProperties {
+
+	public static final String DB_NAME = "bg_db" ;
+	public static final String DB_COLLECTION_USERS = "users" ;
+	public static final String DB_COLLECTION_RESOURCES = "resources" ;
+	public static final String DB_COLLECTION_MANIPULATIONS = "manipulations";
+	public static final String DB_COLLECTION_FRIENDS = "friends";
+
+}
